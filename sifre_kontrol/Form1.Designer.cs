824e6ea0032cd61ced751a07d98352ccd22bab56
sude.karakus <sude.karakus@KDK-103-PC07-YZ>
2023-09-28 13:52:11 +0300

    Add project files.

---
+++ sifre_kontrol/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace sifre_kontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtSifre = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtSifre
+            // 
+            txtSifre.Location = new Point(118, 65);
+            txtSifre.MaxLength = 50;
+            txtSifre.Name = "txtSifre";
+            txtSifre.Size = new Size(365, 23);
+            txtSifre.TabIndex = 0;
+            txtSifre.TextChanged += txtSifre_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(118, 101);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 15);
+            label1.TabIndex = 1;
+            label1.Text = "şifre durumu";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(445, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(19, 15);
+            label2.TabIndex = 2;
+            label2.Text = "50";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(246, 41);
+            label3.Name = "label3";
+            label3.Size = new Size(106, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Şifre Kontrol";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(194, 94);
+            button1.Name = "button1";
+            button1.Size = new Size(201, 31);
+            button1.TabIndex = 4;
+            button1.Text = "OK";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(641, 240);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtSifre);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtSifre;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+    }
+}
---
+++ sifre_kontrol/Form1.cs
@@ -0,0 +1,93 @@
+namespace sifre_kontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        string sifreKontrol;
+        int karaktersy;
+
+        private void txtSifre_TextChanged(object sender, EventArgs e)
+        {
+            string sifre = txtSifre.Text;
+            karaktersy = sifre.Length;
+            int kalanKaraktersy = 50 - karaktersy;
+
+            label2.Text = kalanKaraktersy.ToString();
+            bool ozelKarakterVarmi = false;
+            bool sayiVarmi = false;
+
+            foreach (char s in sifre)
+            {
+                if (!char.IsLetterOrDigit(s))
+                {
+                    //özel karakterse false gelecek
+                    ozelKarakterVarmi = true;
+
+
+                }
+                else if (char.IsDigit(s))
+                {
+                    sayiVarmi = true;
+                }
+
+            }
+
+            if (karaktersy < 6)
+            {
+                sifreKontrol = "zayýf";
+                label1.Text = sifreKontrol;
+                label1.ForeColor= Color.Red;
+
+
+
+            }
+
+            else if (ozelKarakterVarmi == true || sayiVarmi == true)
+            {
+
+                sifreKontrol = "orta";
+                label1.Text = sifreKontrol;
+                label1.ForeColor = Color.Green;
+            }
+
+            if (karaktersy > 6 && ozelKarakterVarmi == true && sayiVarmi == true)
+
+            {
+
+                sifreKontrol = "Güçlü parola";
+                label1.Text = sifreKontrol;
+                label1.ForeColor = Color.Blue;
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (karaktersy < 6)
+            {
+                MessageBox.Show("Þifre kabul edilmedi!");
+            }
+            else
+            {
+                MessageBox.Show("Þifre kabul edildi.");
+            }
+
+            txtSifre.Clear();
+
+
+
+        }
+
+    }
+}
